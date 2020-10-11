1a63eaa3da308691a486b73f5af645e10ef1d8a5
PedroGaN <pgarcianavalon@gmail.com>
2020-10-12 00:44:14 +0100

    TableView, TableCell + basic functions

---
+++ TableView/Classes/CustomCell.swift
@@ -0,0 +1,9 @@
+//
+//  CustomCell.swift
+//  TableView
+//
+//  Created by user177273 on 10/12/20.
+//  Copyright © 2020 PedroGaN. All rights reserved.
+//
+
+import UIKit
--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var data : [String] = ["owo"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+        
+        cell.textLabel?.text = data[indexPath.row]
+        
+        return cell
     }
 
 
